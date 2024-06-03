7bb63935ac3fa80b162713e810d5f8877bb2295a
MC4MP02 <mcampocabrera2013@gmail.com>
2024-06-03 12:53:32 +0200

    frontend DONE

--- src/components/SelectableImageComponent.vue
+++ src/components/SelectableImageComponent.vue
@@ -1,12 +1,29 @@
 <script>
-export default{
-  
+export default {
+    props: ['src', 'modelValue'],
+    emits: ['update:modelValue'],
+    data() {
+        return {
+            border: 'none'
+        }
+    },
+    methods: {
+        handleClick() {
+            this.border = this.border == 'none' ? '4px red solid' : 'none'
+        }
+    },
+    watch: {
+        border(newVal) {
+            if (newVal != 'none') {
+                this.$emit('update:modelValue', true)
+            }
+        }
+    }
 }
 </script>
 
 <template>
-
+    <img :src="src" :style="`border: ${border};`" @click="handleClick">
 </template>
 
-<style>
-</style>
+<style></style>
